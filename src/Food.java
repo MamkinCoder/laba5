d74f48537b850cf27facdbe19af01af9d5c78d3d
MamkinCoder <yarikbahonov@yahoo.com>
2017-04-23 21:04:02 +0300

    Добавил покасеньки

--- src/Food.java
+++ src/Food.java
@@ -5,6 +5,8 @@ public abstract class Food implements Nameable {
     private Date expirationDate;
     private String name;
 
+    int Poka;
+
     Food(TASTE teaste, String name) {
         this.taste = teaste;
         this.name = name;
