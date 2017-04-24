7a718225cd89d62028bea99ccf3e8f4b359c985a
MamkinCoder <yarikbahonov@yahoo.com>
2017-04-24 12:38:34 +0300

    Понедельник

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-
 public class Main {
-    public static void main(String args[]) {
-
-    System.out.println("Hello World" );
+    public static void main(String[] args) {
+        Food f1 = new Food();
+        Food f2 = new Food();
+        f2.setDate(2990, 1, 2);
+        System.out.println(f1.compareTo(f2));
     }
 }
