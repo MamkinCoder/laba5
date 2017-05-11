76ca4dcda01e57957aaf15588da21ecc8e96d1ea
Aleksey Levenets <levenecalexey@gmail.com>
2017-05-11 21:32:17 +0300

    INPUT DONE

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,29 +1,36 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.PriorityQueue;
-import java.util.Scanner;
-import java.io.BufferedOutputStream;
+import java.io.*;
+import java.util.*;
 
 public class FileIO {
 
-
-
-    public void ReadQueue(String fileName) {
-try {
-    File file = new File(fileName);
-    Scanner scanner = new Scanner(file);
-    while (scanner.hasNext()) {
-        System.out.println(scanner.next());
-    }
-    scanner.close();
-}catch (FileNotFoundException kaka){
-    kaka.printStackTrace();
-}
-
+    public PriorityQueue<Food> readQueue(String path) throws FileNotFoundException {
+        PriorityQueue<Food> readed = new PriorityQueue<>();
+        Scanner scanner = new Scanner(new File(path));
+        while (scanner.hasNext()){
+            String line = scanner.nextLine();
+            if (line.equals("")) continue;
+            readed.add(parseFood(line));
+        }
+        return readed;
     }
 
-    public void WriteQueue(String path){
+    private Food parseFood(String line) {
+        Food food = new Food();
+        String[] fields = line.split(",");
+        try {
+            String[] date = fields[0].trim().split("-");
+            food.setDate(Integer.parseInt(date[0]),Integer.parseInt(date[1]),Integer.parseInt(date[2]));
+            food.setTaste(Food.TASTE.valueOf(fields[2].trim()));
+            food.setName(fields[1].trim());
+        } catch (Exception e){
+            System.out.println("Wrong input line : " + line);
+        }
+        return food;
+    }
+
+    public boolean writeQueue(String path, PriorityQueue<Food> data){
+        Boolean result = true;
         //тутачки нужно написать запись
+        return result;
     }
-
 }
