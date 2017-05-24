380c1fdf207866cac2e19d91d167227cae28f930
MamkinCoder <yarikbahonov@yahoo.com>
2017-05-24 07:14:33 +0300

    Добавил файлик IO

---
+++ src/Commands.java
@@ -0,0 +1,5 @@
+/**
+ * Created by yaroslav on 24/05/2017.
+ */
+public class Commands {
+}
---
+++ src/Nameable.java
@@ -0,0 +1,6 @@
+/**
+ * Created by yaroslav on 25/04/2017.
+ */
+public interface Nameable {
+    public String getName();
+}
